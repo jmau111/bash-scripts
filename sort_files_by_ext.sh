67f1e1468ee0b847993f9dfa8f92bee716328863
J. ⭐⭐ <6878027+jmau111@users.noreply.github.com>
2023-02-24 16:21:47 +0100

    update sorting algo
    Signed-off-by: J. ⭐⭐ <6878027+jmau111@users.noreply.github.com>

--- sort_files_by_ext.sh
+++ sort_files_by_ext.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-trap 'echo -e "\033[31mScript failed with an unexpected error!\033[0m" >&2; exit 1' ERR
-
 d="${1:-$PWD}"
 
 if [ ! -d "$d" ]; then
@@ -9,32 +7,23 @@ if [ ! -d "$d" ]; then
   exit 1
 fi
 
-# Edge case when the targeted dir is outside user home dir
-if [[ ! "$d" =~ "$HOME" ]]; then
+if ! realpath --relative-to="$HOME" "$d" >/dev/null 2>&1; then
   echo -e "\033[33mYou asked for $d, but it is not in $HOME.\033[0m"
   read -p "Are you sure you want to proceed? (y/n) " answer
 
   [[ "$answer" == [Yy]* ]] && echo "Ok" || exit 1
 fi
 
-find "$d" -type f -printf '%f\n' | while read -r file; do
-
-  # Don't touch the script itself in case it's executed in the same dir
-  if [ "$file" == "$(basename "$0")" ]; then
-    continue
-  fi
-
-  ext="${file##*.}"
+find "$d" -type f -not -name "$(basename "$0")" -print0 | while IFS= read -r -d '' file; do
   
-  if [ ! -z "$ext" ]; then
-    if [ ! -d "$ext" ]; then
-      mkdir "$ext"
-    fi
-    if [ ! -f "$ext/$file" ]; then
-        mv "$file" "$ext/$file"
-    fi
+  ext=$(awk -F. '{print tolower($NF)}' <<< "$file")
+  _d="$d/$ext"
+  if [ ! -d "$_d" ]; then
+    mkdir "$_d"
   fi
-
+  if [ ! -f "$_d/$(basename "$file")" ]; then
+    mv "$file" "$_d/"
+  fi
 done
 
 echo "Done."
